690add87d2107523afa63fc300163edd7ed3e7a9
shangcj@unitedcloud.net <shangcj@unitedcloud.net>
2020-04-15 10:24:49 +0800

    测试自定义class编译实现

--- src/main/java/com/common/ClassDefiner.java
+++ src/main/java/com/common/ClassDefiner.java
@@ -1,6 +1,12 @@
 package com.common;
 
+import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import org.apache.dubbo.common.compiler.Compiler;
+import org.apache.dubbo.common.compiler.support.JdkCompiler;
 
 import sun.misc.Unsafe;
 
@@ -18,25 +24,63 @@ public class ClassDefiner {
 	}
 	public static void main(String[] args) {
 		
-		String code="public class A{"
-				+ "private String name=\"xx\";"
-				+ "private int age;"
-				+ "public A(){"
-				+	"System.out.println(this);"
-				+ "}"
+		String code=""
+				+ "public class A {"
+				+" static{"
+				+ " System.out.println(\"abc\");"
+				+"}"
+				+ "private String name=\"xx\"; "
+				+ "private int age; "
+				+ "public A(){ "
+				+	"System.out.println(this); "
+				+ "} "
 				+"public String getName(){"
-				+ "return this.name}"
+				+ "return this.name;}"
 				+ "}";
 		Class cls=ClassDefiner.class;
 		try {
 			Class.forName("A", true, org.apache.dubbo.common.utils.ClassUtils.getCallerClassLoader(cls));
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+//			e.printStackTrace();
 			ClassLoader cloader=Thread.currentThread().getContextClassLoader();
 			//public native Class<?> defineClass(String name, byte[] b, int off, int len, ClassLoader loader, ProtectionDomain protectionDomain);
-			cls=unsafe.defineClass("A", code.getBytes(), 0, code.getBytes().length, cloader,cls.getProtectionDomain());
+			//Exception in thread "main" java.lang.ClassFormatError: Incompatible magic value 1885430635 in class file A
+			//cls=unsafe.defineClass("A", code.getBytes(), 0, code.getBytes().length, cloader,cls.getProtectionDomain());
 		}
 		
+		//ExtensionLoader
+//		java.lang.Compiler
+//		Compiler
+		//JavaCompiler
+		//jsp如果被重新编译?
+		//java动态代理如何生成的字节码
+		//Proxy
+		//javax.annotation.processing.Processor
+		
+		Compiler compiler=new JdkCompiler();
+		try {
+			Class<?> aclazz=compiler.compile(code, ClassDefiner.class.getClassLoader());
+			System.out.println(aclazz);
+			Stream.of(aclazz.getDeclaredMethods())
+				.forEach(x->System.out.println(x.toString()));
+			
+			aclazz.newInstance();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//JavacTaskImpl
+		System.out.println(CLASS_PATTERN.matcher("class A ").matches());
 					
 	}
+	static final Pattern CLASS_PATTERN = Pattern.compile("class\\s+([$_a-zA-Z][$_a-zA-Z0-9]*)\\s+");
+}
+
+//jls8 9.6 Annotation Types 两个独立的tokens（distinct tokens）他们之前可包含空格，但不建议这么做。
+@ interface AAA {
+	
+}
+
+enum BBB implements Serializable{
+	
 }
